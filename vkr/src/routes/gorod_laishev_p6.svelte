4d1e513627a1a2394a5ca808b1214c8da2ec6d27
Kamila Shagimardanova <114108569+kamilories@users.noreply.github.com>
2024-05-06 17:11:14 +0300

    закончен раздел город лаишев, добавлены данные по последнему разделу

--- vkr/src/routes/gorod_laishev_p6.svelte
+++ vkr/src/routes/gorod_laishev_p6.svelte
@@ -1,1 +1,48 @@
-страница 6
+<script>
+    // Ваши переменные, функции и логика здесь
+    import textData from '../data/data.json';
+    let page_json = textData.gorod_laishev;
+    let inf=page_json.vnutri_punktov[3];
+    let zagolovok=inf.zagolovok;
+    let text=inf.text;
+    let pict=inf.img;
+</script>
+
+<style>
+    /* Ваши стили здесь */
+    .zagolovok{
+        position: absolute;
+        font-family:'KuzanyanC';
+        top: 22%;
+        left: 63%;
+        max-width: 100%;
+        color: #663300;
+        font-size: 3.6vh;
+    }
+    .text_history{
+        position: absolute;
+        font-family: "PT Sans Caption";
+        top:28%;
+        left: 63%;
+        width: 32%;
+        max-width: 100%;
+        word-wrap: break-word;
+        white-space: pre-wrap;
+        font-size: 1.9vh;
+    }
+    .foto{
+        position: absolute;
+        left: 3%;
+        top:20%;
+        width: 74%;
+        max-width: 100%;
+    }
+</style>
+
+<div>
+    <!-- Ваш контент здесь -->
+    <img class="foto" src={pict} alt="home"> 
+    <h1 class="zagolovok">{zagolovok}</h1>
+    <pre class="text_history">{text}</pre>
+
+</div>
--- vkr/src/routes/gorod_laishev_p7.svelte
+++ vkr/src/routes/gorod_laishev_p7.svelte
@@ -1,1 +1,48 @@
-страница 7
+<script>
+    // Ваши переменные, функции и логика здесь
+    import textData from '../data/data.json';
+    let page_json = textData.gorod_laishev;
+    let inf=page_json.vnutri_punktov[4];
+    let zagolovok=inf.zagolovok;
+    let text=inf.text;
+    let pict=inf.img;
+</script>
+
+<style>
+    /* Ваши стили здесь */
+    .zagolovok{
+        position: absolute;
+        font-family:'KuzanyanC';
+        top: 18%;
+        left: 35%;
+        max-width: 100%;
+        color: #663300;
+        font-size: 4vh;
+    }
+    .text_history{
+        position: absolute;
+        font-family: "PT Sans Caption";
+        top:23%;
+        left: 10%;
+        width: 85%;
+        max-width: 100%;
+        word-wrap: break-word;
+        white-space: pre-wrap;
+        font-size: 2.5vh;
+    }
+    .foto{
+        position: absolute;
+        left: 10%;
+        top:40%;
+        width: 80%;
+        max-width: 100%;
+    }
+</style>
+
+<div>
+    <!-- Ваш контент здесь -->
+    <img class="foto" src={pict} alt="home"> 
+    <h1 class="zagolovok">{zagolovok}</h1>
+    <pre class="text_history">{text}</pre>
+
+</div>
